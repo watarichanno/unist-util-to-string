c2236f658b67da1c44bcdbdcba2c3d2cf6a680fa
watarichanno <watarichanno@gmail.com>
2026-02-02 23:16:52 +0700

    Set up eslint and code quality npm scripts

---
+++ eslint.config.ts
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  eslintConfigPrettier,
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import type { Node } from "unist";
 
-interface Options {}
+interface Options {
+  handlers: string;
+}
 
 export function toString(tree: Node, options: Options): string {
   return `${JSON.stringify(tree)}\n${JSON.stringify(options)}`;
